e63bf70875607494c33a8590ca1a9077c6b4102d
saadousman <tuan@likemindsconsulting.com>
2023-02-06 22:07:58 +0530

    create artifact registry
    inital commit

---
+++ artifact-registry/artifact-registry.tf
@@ -0,0 +1,9 @@
+resource "google_artifact_registry_repository" "my-repo" {
+  provider = google-beta
+
+  location = "northamerica-notheast1"
+  repository_id = "likeminds-ping-images"
+  description = "to host custom docker images created by google cloud build to be used by ping pods on GKE"
+  format = "DOCKER"
+#   kms_key_name = "KEY"
+}
